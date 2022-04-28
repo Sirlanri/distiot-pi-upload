a4e261ba1dd1a3fed7e010559f5d9a5765f5d9b6
ricoPi <mail@ri-co.cn>
2022-04-28 14:44:49 +0800

    测试通过 正常采集温湿度和内核温度

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"strconv"
+	"time"
 
+	"github.com/Sirlanri/distiot-pi-upload/sdk"
 	"github.com/d2r2/go-dht"
 )
 
-func gethumi() {
+func gethumi() (int, int, error) {
 	//引脚号使用的物理号
 	temperature, humidity, err :=
 		dht.ReadDHTxx(dht.DHT11, 4, false)
 	if err != nil {
-		fmt.Println(err.Error())
+		return 0, 0, err
 	}
-	if temperature != -1 {
-		// Print temperature and humidity
-		fmt.Printf("Temperature = %v*C, Humidity = %v%%\n",
-			temperature, humidity)
+	if temperature == -1 {
+		return 0, 0, errors.New("采集温湿度失败")
 	}
+	return int(temperature), int(humidity), nil
 
 }
 
@@ -40,6 +42,15 @@ func getCodeTemp() float64 {
 }
 
 func main() {
-	coreTemp := getCodeTemp()
-	fmt.Println(coreTemp)
+
+	for {
+		time.Sleep(time.Second)
+		coreTemp := getCodeTemp()
+		fmt.Println("内核温度 ", coreTemp)
+		temp, humi, err := gethumi()
+		if err != nil {
+			continue
+		}
+		fmt.Printf("潮湿为%d，温度为%d \n", humi, temp)
+	}
 }
